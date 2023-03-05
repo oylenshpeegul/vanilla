eb3ebb32438d020d76f915d9196d0d8818569f5c
Tim Heaney <oylenshpeegul@proton.me>
2023-03-05 16:55:44 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const H1 = document.querySelector("h1")
+const Text = document.querySelector("#text")
+const Buttons = document.querySelector("#buttons")
+
+function start() {
+    
+    H1.textContent = "Vanilla";
+
+    Text.innerHTML = "vanilla";
+
+}
