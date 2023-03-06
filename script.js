91feb75114e62db403d4d31839ac5a404de28758
Tim Heaney <oylenshpeegul@proton.me>
2023-03-06 08:32:34 -0500

    three columns

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 
-function reliquor() {
-    let b = parseInt(document.getElementById("beans").value);
+function beans() {
+    let b = parseFloat(document.getElementById("beans").value);
     document.getElementById("liquor").value = 8 * b;
+    document.getElementById("double").value = 4 * b;
 }
 
-function rebeans() {
-    let l = parseInt(document.getElementById("liquor").value);
-    document.getElementById("beans").value = l /8;
+function liquor() {
+    let l = parseFloat(document.getElementById("liquor").value);
+    document.getElementById("beans").value = l / 8;
+    document.getElementById("double").value = l / 2;
+}
+
+function double_fold() {
+    let d = parseFloat(document.getElementById("double").value);
+    document.getElementById("beans").value = d / 4;
+    document.getElementById("liquor").value = 2 * d;
 }
