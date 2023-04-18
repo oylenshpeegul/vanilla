51e742a1f00134ac446d32a4818b1b1003ab3f07
Tim Heaney <oylenshpeegul@proton.me>
2023-04-18 17:59:24 -0400

    add 1.5 fold

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 
 function beans() {
     let b = parseFloat(document.getElementById("beans").value);
-    document.getElementById("liquor").value = 8 * b;
-    document.getElementById("double").value = 4 * b;
+    document.getElementById("liquor").value = b * 8;
+    document.getElementById("oneandahalf").value = b * 6;
+    document.getElementById("double").value = b * 4;
 }
 
 function liquor() {
     let l = parseFloat(document.getElementById("liquor").value);
     document.getElementById("beans").value = l / 8;
-    document.getElementById("double").value = l / 2;
+    document.getElementById("oneandahalf").value = l * (6 / 8);
+    document.getElementById("double").value = l * (4 / 8)
+}
+
+function one_and_a_half_fold() {
+    let o = parseFloat(document.getElementById("oneandahalf").value);
+    document.getElementById("beans").value = o / 6;
+    document.getElementById("liquor").value = o * (8 / 6);
+    document.getElementById("double").value = o * (4 / 6);
 }
 
 function double_fold() {
     let d = parseFloat(document.getElementById("double").value);
     document.getElementById("beans").value = d / 4;
-    document.getElementById("liquor").value = 2 * d;
+    document.getElementById("liquor").value = d * (8 / 4);
+    document.getElementById("oneandahalf").value = d * (6 / 4);
 }
